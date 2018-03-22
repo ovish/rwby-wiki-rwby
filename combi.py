8a8299c58a1aa84ca88f45caf67332f880b5346f
ovish <36478032+ovish@users.noreply.github.com>
2018-03-22 23:17:55 +0900

    add:combi中身作成

--- combi.py
+++ combi.py
@@ -5,3 +5,39 @@ import re
 import sys
 import os
 import codecs
+
+
+with open('j1-prrg.md', 'r') as f:
+    listj1 = f.read()
+with open('j2-smry.md', 'r') as f:
+    listj2 = f.read()
+with open('j3-tril.md', 'r') as f:
+    listj3 = f.read()
+with open('j4-epsd.md', 'r') as f:
+    listj4 = f.read()
+with open('j5-medi.md', 'r') as f:
+    listj5 = f.read()
+with open('j6-dvlp.md', 'r') as f:
+    listj6 = f.read()
+with open('j7-trva.md', 'r') as f:
+    listj7 = f.read()
+
+listCombi = str(
+    listj1
+    + "\n"
+    + listj2
+    + "\n"
+    + listj3
+    + "\n"
+    + listj4
+    + "\n"
+    + listj5
+    + "\n"
+    + listj6
+    + "\n"
+    + listj7
+    )
+
+print(listCombi)
+with open('combi.md', 'w') as f:
+    f.write(listCombi)

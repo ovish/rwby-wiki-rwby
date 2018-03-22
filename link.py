f63b61484142b0b0171d7e20b96dfae4cf8a599b
ovish <36478032+ovish@users.noreply.github.com>
2018-03-22 23:48:47 +0900

    add:link.pyを追加

--- link.py
+++ link.py
@@ -5,3 +5,18 @@ import re
 import sys
 import os
 import codecs
+
+with open('combi.md', 'r') as f:
+    strJpn = f.read()
+with open('source.md', 'r') as f:
+    strLink = f.read()
+
+listJpn = re.split('\[\^\d+\]', strJpn)
+listLink = re.findall('<ref.*?>.+?<\/ref>', strLink)
+
+listSource = [
+    lineJpn + lineLink
+    for i, (lineJpn, lineLink)
+    in enumerate(zip(listJpn, listLink))
+    ]
+print(listLink)
